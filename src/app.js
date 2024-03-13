b07dbd2978c2a0cc5084dd681b2a3afa1ebde173
friend-0228 <kwj2712@gamil.com>
2024-03-13 11:38:51 +0900

    FEAT : cors 처리를 위한 코드 추가

--- src/app.js
+++ src/app.js
@@ -8,6 +8,11 @@ app.use(express.json());
 
 app.use('/', [postsRouter]);
 
+app.use((req, res) => {
+    res.header('Access-Control-Allow-Origin', '*'); // 모든 도메인 허용
+    res.header('Access-Control-Allow-Origin', 'http://13.209.22.133'); // 특정 IPv4 퍼블릭 주소 허용
+});
+
 app.listen(PORT, () => {
     console.log(PORT, '포트로 서버가 열렸어요!');
 });
