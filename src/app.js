0946f7325a15c8cb7e51f45628951264bfec3076
LetyPark <sunghee.leticia@gmail.com>
2024-03-13 14:46:16 +0900

    refactor : 헤헷.. 중복 수정

--- src/app.js
+++ src/app.js
@@ -23,8 +23,6 @@ app.use((req, res, next) => {
 app.use('/', [postsRouter, usersRouter]);
 
 
-app.use('/', [postsRouter]);
-
 app.listen(PORT, () => {
     console.log(`${PORT} 포트로 서버가 열렸어요!`);
 });
