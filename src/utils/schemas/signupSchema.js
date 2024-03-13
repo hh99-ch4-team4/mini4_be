9b0b8c7a5c0f5adbc211cca8d196ce1b9dc6efe2
LetyPark <sunghee.leticia@gmail.com>
2024-03-14 00:19:28 +0900

    Refactor

--- src/utils/schemas/signupSchema.js
+++ src/utils/schemas/signupSchema.js
@@ -1,11 +1,14 @@
 import Joi from 'joi';
 
 const signUpSchema = Joi.object({
-    userName : Joi.string().min(3).max(15).alphanum().required(), 
-    nickname: Joi.string().min(3).max(15).required(), 
+    email: Joi.string().email().required(),
+    nickname: Joi.string().min(3).max(15)
+    .required(), 
     password: Joi.string() 
         .min(6)
         .max(20)
+        .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])'))
+        .message('비밀번호는 최소한 하나의 대문자, 소문자, 숫자를 포함해야 합니다.')
         .required(),
 });
 
