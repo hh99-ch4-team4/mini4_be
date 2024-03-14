d770d4d617bd63d3fa30393ee89cbc8013a2e617
friend-0228 <kwj2712@gamil.com>
2024-03-14 18:30:12 +0900

    REFACTOR : 댓글 CRUD 수정

--- src/routes/likes.router.js
+++ src/routes/likes.router.js
@@ -6,26 +6,46 @@ const router = express.Router();
 
 // 좋아요 API
 router.post('/posts/:postId/likes', authMiddleware, async (req, res, next) => {
-    const { postId } = req.params;
-    const userId = req.user.id;
+    try {
+        const { postId } = req.params;
+        const userId = req.user.id;
 
-    if (!postId || !userId) return res.status(400).json({ message: '데이터 형식이 올바르지 않습니다.' });
+        if (!postId || !userId) return res.status(400).json({ message: '데이터 형식이 올바르지 않습니다.' });
 
-    const post = await prisma.posts.findFirst({ where: { id: +postId } });
-    if (!post) return res.status(404).json({ message: '존재하지 않는 게시글입니다.' });
+        const post = await prisma.posts.findFirst({ where: { id: +postId } });
+        if (!post) return res.status(404).json({ message: '존재하지 않는 게시글입니다.' });
 
-    const like = await prisma.likes.findFirst({
-        where: { postId: +postId, userId: +userId },
-    });
+        // 좋아요를 처음 누르는건지 아닌지에 따른 좋아요의 상태 찾기
+        // : 좋아요를 한 번도 누르지 않았다면 like는 없을 것이고, 누른적이 있다면 like가 있을 것이다.
+        const like = await prisma.likes.findFirst({
+            where: { postId: +postId, userId: +userId },
+        });
 
-    if (!like) {
-        // 좋아요 추가
-        await prisma.likes.create({ data: { postId: +postId, userId: +userId, likeCheck: true } });
-        return res.status(200).json({ message: '현재 게시글에 좋아요를 눌렀습니다.' });
-    } else {
-        // 좋아요 삭제
-        await prisma.likes.delete({ where: { id: like.id } });
-        return res.status(200).json({ message: '현재 게시물에 대한 좋아요를 취소했습니다.' });
+        let responseMessage = '';
+        if (like) {
+            // 좋아요를 처음 누른게 아닌데 또 좋아요를 누르는 상황이라면, 좋아요 값을 전환하여 반환
+            const updatedLike = await prisma.likes.update({
+                where: { id: like.id },
+                data: { likeCheck: !like.likeCheck },
+            });
+            responseMessage = updatedLike.likeCheck ? '좋아요를 등록했습니다' : '좋아요를 취소했습니다.';
+        } else {
+            // 좋아요를 처음 누른다면, 좋아요 등록.
+            await prisma.likes.create({
+                data: { postId: +postId, userId: +userId, likeCheck: true },
+            });
+            responseMessage = '좋아요를 등록했습니다.';
+        }
+
+        // 좋아요 개수 count
+        const likeCount = await prisma.likes.count({
+            where: { postId: +postId, likeCheck: true },
+        });
+
+        return res.status(200).json({ message: responseMessage, likeCount });
+    } catch (error) {
+        console.error(error);
+        // next(error);
     }
 });
 
